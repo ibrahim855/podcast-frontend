f424428c4662c5d2fc7d31ec2c66cda3ddcd221d
ibrahim855 <78034840+ibrahim855@users.noreply.github.com>
2021-11-19 18:22:00 +0100

    nice

--- src/components/UI/DurationBar/DurationBar.js
+++ src/components/UI/DurationBar/DurationBar.js
@@ -7,10 +7,14 @@ import classes from './DurationBar.module.css';
 function DurationBar(props) {
     const { percentage } = props;
 
+
+
     return <React.Fragment>
              <div style={{width: percentage + "%",
-                      transition: "all 3s ease"}} className={classes.barPodcast}>
-          </div>
+                      transition: "all 3s ease"}}
+                   className={classes.barPodcast}
+                   >
+             </div>
           <div className={classes.barBackground}>
 
           </div>
