63fd90d17ed15a4787bc5b9d3e1dbc4290c75b9b
ibrahim855 <78034840+ibrahim855@users.noreply.github.com>
2021-11-19 20:33:49 +0100

    nice

--- src/utility/baseURL.js
+++ src/utility/baseURL.js
@@ -1,4 +1,5 @@
 
- export const URL = "http://localhost:8000";
+//  export const URL = "http://localhost:8000";
 
-//  export const URL = "https://backendpodcast.herokuapp.com";
+ export const URL = "https://another-podcast-backend.herokuapp.com";
+ 
